37466d09f82aaecc0ec60959036dd4b7139d8f6a
roll <eskarev@gmail.com>
2021-06-04 16:42:12 +0200

    Added sidebar to the left

--- livemark/assets/templates/sidebar.js
+++ livemark/assets/templates/sidebar.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Where to grab the headings to build the table of contents.
     contentSelector: ".markdown-body",
     // Which headings to grab inside of the contentSelector element.
-    headingSelector: "h1, h2, h3",
+    headingSelector: "h2, h3, h4",
     // For headings inside relative or absolute positioned containers within content.
     hasInnerContainers: true,
   });
