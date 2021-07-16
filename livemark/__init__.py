cef96e4300dc06fe90c5e4f6c86a9bfec7219fe6
roll <eskarev@gmail.com>
2021-07-16 16:52:17 +0300

    Bootstrapped plugins

--- livemark/__init__.py
+++ livemark/__init__.py
@@ -1,5 +1,6 @@
 from .config import Config
 from .document import Document
+from .plugin import Plugin
 from .processor import Processor
 from .program import program
 from .project import Project
---
+++ livemark/plugin.py
@@ -0,0 +1,12 @@
+class Plugin:
+    def process_html(self, html):
+        pass
+
+    def process_code(self, code, *, header):
+        pass
+
+    def process_config(self, config):
+        pass
+
+    def process_document(self, document):
+        pass
