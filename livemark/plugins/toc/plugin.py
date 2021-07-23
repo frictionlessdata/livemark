6cb91c76f954cd52113ddcfb8cdc532f0def01f4
roll <eskarev@gmail.com>
2021-07-23 16:29:59 +0300

    Updated TocPlugin

--- livemark/plugins/toc/plugin.py
+++ livemark/plugins/toc/plugin.py
@@ -2,5 +2,7 @@ from ...plugin import Plugin
 
 
 class TocPlugin(Plugin):
-    def process_html(self, html):
-        html("#livemark-left").append(self.read_asset("markup.html"))
+    def process_html(self, markup):
+        markup.query("head").append(self.read_asset("style.css", tag="style"))
+        markup.query("body").append(self.read_asset("script.js", tag="script"))
+        markup.query("#livemark-right").append(self.read_asset("markup.html"))
---
+++ livemark/plugins/toc/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  tocbot.init({
+    // Where to render the table of contents.
+    tocSelector: ".js-toc",
+    // Where to grab the headings to build the table of contents.
+    contentSelector: "#livemark-main",
+    // Which headings to grab inside of the contentSelector element.
+    headingSelector: "{{ livemark.navigation.selector or 'h2, h3' }}",
+    // For headings inside relative or absolute positioned containers within content.
+    hasInnerContainers: true,
+    // Called each time a heading is parsed. Expects a string in return.
+    headingLabelCallback: (label) => label.replace(/(^#|#$)/g, "").trim(),
+  });
+});
