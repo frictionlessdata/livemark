1c11d758ca3e73ff2277dc6e16a52fb5ab863afb
roll <eskarev@gmail.com>
2021-07-23 16:05:59 +0300

    Updated FlowPlugin

--- livemark/plugins/flow/plugin.py
+++ livemark/plugins/flow/plugin.py
@@ -2,5 +2,10 @@ from ...plugin import Plugin
 
 
 class FlowPlugin(Plugin):
-    def process_html(self, html):
-        html("#livemark-main").append(self.read_asset("markup.html"))
+    def process_html(self, markup):
+        markup.query("head").append(self.read_asset("style.css"))
+        markup.query("#livemark-main").append(
+            # TODO: implement prev/next
+            self.read_asset("markup.html"),
+            data={"prev": {}, "next": {}},
+        )
