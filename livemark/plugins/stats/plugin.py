2a116e8f73e07ea1333e6ff31d29ee6e39ef8f1d
roll <eskarev@gmail.com>
2021-07-23 16:21:39 +0300

    Updated StatsPlugin

--- livemark/plugins/stats/plugin.py
+++ livemark/plugins/stats/plugin.py
@@ -1,6 +1,14 @@
+import datetime
 from ...plugin import Plugin
 
 
 class StatsPlugin(Plugin):
-    def process_html(self, html):
-        html("#livemark-left").append(self.read_asset("markup.html"))
+    def process_html(self, markup):
+        config = markup.document.config
+        markup.query("head").append(self.read_asset("style.css", tag="style"))
+        markup.query("#livemark-left").append(
+            self.read_asset(
+                "markup.html",
+                data={"current": datetime.datetime.now(), "format": config.stats.format},
+            )
+        )
