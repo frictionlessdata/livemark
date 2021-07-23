730579adfaa68efb55bed5b521e7b4b216c3adf8
roll <eskarev@gmail.com>
2021-07-23 16:03:30 +0300

    Updated BrandPlugin

--- livemark/plugins/about/plugin.py
+++ livemark/plugins/about/plugin.py
@@ -3,13 +3,8 @@ from ...plugin import Plugin
 
 class AboutPlugin(Plugin):
     def process_markup(self, markup):
+        config = markup.document.config
         markup.query("head").append(self.read_asset("style.css"), tag="style")
         markup.query("#livemark-right").append(
-            self.read_asset(
-                "markup.html",
-                data={
-                    # TODO: make self.config available using "process_document/config"?
-                    "description": markup.document.config.about.description,
-                },
-            )
+            self.read_asset("markup.html", data={"description": config.about.description})
         )
--- livemark/plugins/brand/plugin.py
+++ livemark/plugins/brand/plugin.py
@@ -2,5 +2,9 @@ from ...plugin import Plugin
 
 
 class BrandPlugin(Plugin):
-    def process_html(self, html):
-        html("#livemark-left").append(self.read_asset("markup.html"))
+    def process_html(self, markup):
+        config = markup.document.config
+        markup.query("head").append(self.read_asset("style.css"))
+        markup.query("#livemark-left").append(
+            self.read_asset("markup.html"), data={"title": config.brand.title}
+        )
