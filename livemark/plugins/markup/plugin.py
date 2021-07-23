480c9f6cee6d345eb7a3f68fa7755ef9d1afe349
roll <eskarev@gmail.com>
2021-07-23 11:48:28 +0300

    Implemented MarkupPlugin

---
+++ livemark/plugins/markup/plugin.py
@@ -0,0 +1,23 @@
+import bs4
+import marko
+from marko.ext.gfm import GFM
+from ...plugin import Plugin
+
+
+class MarkupPlugin(Plugin):
+    def process_snippet(self, snippet):
+        if snippet.format == "html":
+            if "markup" in snippet.header:
+                markdown = marko.Markdown()
+                markdown.use(GFM)
+                # TODO: activate
+                #  markdown.use(HtmlExtension)
+                html = bs4.BeautifulSoup(snippet.input, features="html.parser")
+                for node in html.select(".markdown"):
+                    if len(node.contents) == 1:
+                        text = node.contents[0]
+                        if isinstance(text, bs4.element.NavigableString):
+                            text = markdown.convert(text)
+                            inner = bs4.BeautifulSoup(text, features="html.parser")
+                            node.string.replace_with(inner)
+                snippet.output = str(html)
