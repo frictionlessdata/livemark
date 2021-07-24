1eb7aabc7d7c0821b7e399d6afad93173c1c98da
roll <eskarev@gmail.com>
2021-07-24 09:48:00 +0300

    Fixed livemark start

--- livemark/program.py
+++ livemark/program.py
@@ -97,13 +97,14 @@ def program_sync(
 
 @program.command(name="start")
 def program_start(
-    path: str = typer.Argument("index.md", help="Path to markdown"),
+    source: str = typer.Argument("index.md", help="Path to markdown"),
+    target: str = typer.Option(settings.DEFAULT_TARGET, help="Path to target"),
 ):
     """Start a Livemark server."""
-    program_build(path, False)
+    program_build(source, target, False)
     server = Server()
     server.watcher.watch(".", delay=1)
-    server.watch(path, partial(program_build, path, False))
+    server.watch(source, partial(program_build, source, target, False))
     server.serve(host="localhost", port=7000, root=".", open_url_delay=1)
 
 
