280acae01a2a42997bea26e30d0e9c941c05306f
roll <eskarev@gmail.com>
2021-07-24 12:02:43 +0300

    Returned tocbot

--- livemark/plugins/toc/plugin.py
+++ livemark/plugins/toc/plugin.py
@@ -3,6 +3,12 @@ from ...plugin import Plugin
 
 class TocPlugin(Plugin):
     def process_markup(self, markup):
+        markup.query("head").append(
+            '<link rel="stylesheet" href="https://unpkg.com/tocbot@4.12.3/dist/tocbot.css">'
+        )
         markup.query("head").append(self.read_asset("style.css", tag="style"))
+        markup.query("body").append(
+            '<script src="https://unpkg.com/tocbot@4.12.3/dist/tocbot.min.js"></script>'
+        )
         markup.query("body").append(self.read_asset("script.js", tag="script"))
         markup.query("#livemark-left").append(self.read_asset("markup.html"))
