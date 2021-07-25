6bcec4ccee07888237210b0f0d6be57d2c06ba72
roll <eskarev@gmail.com>
2021-07-25 11:24:14 +0300

    Recovered FlowPlugin

--- livemark/plugins/flow/plugin.py
+++ livemark/plugins/flow/plugin.py
@@ -5,11 +5,29 @@ class FlowPlugin(Plugin):
     priority = 50
 
     def process_markup(self, markup):
+        pages_config = markup.document.config.get("pages")
+
+        # Prepare prev/next
+        prev = None
+        next = None
+        if pages_config:
+            current_path = "/"
+            current_number = None
+            if markup.document.target != "index.html":
+                current_path = markup.document.target
+            for number, link in enumerate(pages_config["list"], start=1):
+                if link["path"] == current_path:
+                    current_number = number
+            if current_number > 1:
+                prev = pages_config["list"][current_number - 2]
+            if current_number < len(pages_config["list"]):
+                next = pages_config["list"][current_number]
+
+        # Update markup
         markup.add_style("style.css")
         markup.add_markup(
             "markup.html",
             target="#livemark-main",
-            # TODO: implement prev/next
-            prev={},
-            next={},
+            prev=prev,
+            next=next,
         )
--- livemark/plugins/pages/plugin.py
+++ livemark/plugins/pages/plugin.py
@@ -5,10 +5,17 @@ class PagesPlugin(Plugin):
     priority = 90
 
     def process_markup(self, markup):
+
+        # Prepare current
+        current = "/"
+        if markup.document.target != "index.html":
+            current = markup.document.target
+
+        # Update markup
         markup.add_style("style.css")
         markup.add_markup(
             "markup.html",
             target="#livemark-left",
             config=markup.plugin_config,
-            current=markup.document.source,
+            current=current,
         )
