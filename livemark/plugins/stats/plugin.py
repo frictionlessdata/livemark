1885b6dd490b02e3094be542933ff5756e61d44e
roll <eskarev@gmail.com>
2021-07-26 10:26:22 +0300

    Fixed stats plugin

--- livemark/plugins/stats/plugin.py
+++ livemark/plugins/stats/plugin.py
@@ -1,14 +1,20 @@
-import datetime
+import os
+from datetime import datetime
 from ...plugin import Plugin
 
 
 class StatsPlugin(Plugin):
     priority = 60
 
+    # TODO: move gtag to the head and make async?
     def process_markup(self, markup):
+
+        # Prepare code/current
         code = markup.plugin_config["analytics"]["code"]
+        current = datetime.fromtimestamp(os.path.getmtime(markup.document.source))
+
+        # Update markup
         markup.add_style("style.css")
-        # TODO: move to the head and make async?
         markup.add_script(f"https://www.googletagmanager.com/gtag/js?id={code}")
         markup.add_script("script.js", config=markup.plugin_config)
         markup.add_markup(
@@ -16,5 +22,5 @@ class StatsPlugin(Plugin):
             action="prepend",
             target="#livemark-main",
             config=markup.plugin_config,
-            current=datetime.datetime.now(),
+            current=current,
         )
