81fc0073a61d0325d4214cbeacc2909d7b017721
roll <eskarev@gmail.com>
2021-07-28 14:10:26 +0300

    Renamed chore -> hooks

--- livemark/plugins/hooks/plugin.py
+++ livemark/plugins/hooks/plugin.py
@@ -2,11 +2,11 @@ import subprocess
 from ...plugin import Plugin
 
 
-class ChorePlugin(Plugin):
+class HooksPlugin(Plugin):
     def prepare_document(self, document):
-        for code in document.config.get("chore", {}).get("prepare", []):
+        for code in document.plugin_config.get("prepare", []):
             subprocess.run(code, shell=True)
 
     def cleanup_document(self, document):
-        for code in document.config.get("chore", {}).get("cleanup", []):
+        for code in document.plugin_config.get("cleanup", []):
             subprocess.run(code, shell=True)
