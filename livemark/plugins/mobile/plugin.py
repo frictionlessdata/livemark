2aa4bb16152f780b7453d72e95bd8c3c575d6490
roll <eskarev@gmail.com>
2021-08-06 18:47:02 +0300

    Added a comment

--- livemark/plugins/mobile/plugin.py
+++ livemark/plugins/mobile/plugin.py
@@ -1,6 +1,10 @@
 from ...plugin import Plugin
 
 
+# This plugin is based on this article:
+# https://dev.to/devggaurav/let-s-build-a-responsive-navbar-and-hamburger-menu-using-html-css-and-javascript-4gci
+
+
 class MobilePlugin(Plugin):
     # TODO: update all the priorities (maybe group by column/section?)
     priority = 55
