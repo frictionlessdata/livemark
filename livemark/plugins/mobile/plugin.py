87bdb5c3b6ae4a1910ca4f90c39e6dff9176b072
roll <eskarev@gmail.com>
2021-08-06 21:37:04 +0300

    Fixed mobile menu close button

--- livemark/plugins/mobile/plugin.py
+++ livemark/plugins/mobile/plugin.py
@@ -7,15 +7,11 @@ from ...plugin import Plugin
 
 class MobilePlugin(Plugin):
     # TODO: update all the priorities (maybe group by column/section?)
-    priority = 55
+    priority = 10
 
     def process_markup(self, markup):
 
         # Update markup
         markup.add_style("style.css")
         markup.add_script("script.js")
-        markup.add_markup(
-            "markup.html",
-            action="prepend",
-            target="#livemark-main",
-        )
+        markup.add_markup("markup.html")
