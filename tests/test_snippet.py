3b654e4fda91c350b2f89e632ce88328c3f022fb
roll <eskarev@gmail.com>
2021-08-07 16:01:02 +0300

    Fixed chart/table's counters

--- tests/test_snippet.py
+++ tests/test_snippet.py
@@ -7,6 +7,7 @@ from livemark import Snippet
 def test_snippet():
     snippet = Snippet("input", header=["python"])
     assert snippet.header == ["python"]
+    assert snippet.lang == "python"
     assert snippet.input == "input"
     assert snippet.output is None
 
@@ -15,5 +16,6 @@ def test_snippet_update_output():
     snippet = Snippet("input", header=["python"])
     snippet.output = "output"
     assert snippet.header == ["python"]
+    assert snippet.lang == "python"
     assert snippet.input == "input"
     assert snippet.output == "output"
