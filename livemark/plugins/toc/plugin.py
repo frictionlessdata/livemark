5cdb12ddada5ce4477a468c2b146fa8f99cf85aa
roll <eskarev@gmail.com>
2021-08-14 10:37:51 +0300

    Updated toc styles

--- livemark/plugins/toc/plugin.py
+++ livemark/plugins/toc/plugin.py
@@ -18,7 +18,6 @@ class TocPlugin(Plugin):
         selector = self.config.get("selector", "h2, h3")
 
         # Update markup
-        markup.add_style("https://unpkg.com/tocbot@4.12.3/dist/tocbot.css")
         markup.add_style("style.css")
         markup.add_script("https://unpkg.com/tocbot@4.12.3/dist/tocbot.min.js")
         markup.add_script("script.js", selector=selector)
