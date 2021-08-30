37168e0ed5d7d6c74af961c142c5ef2a8193a8d7
roll <eskarev@gmail.com>
2021-08-30 09:58:59 +0300

    Added BlogPlugin.process_project

--- livemark/plugins/blog/plugin.py
+++ livemark/plugins/blog/plugin.py
@@ -1,4 +1,7 @@
+import os
+import glob
 from ...plugin import Plugin
+from ...document import Document
 
 
 class BlogPlugin(Plugin):
@@ -7,16 +10,27 @@ class BlogPlugin(Plugin):
         "type": "object",
         "properties": {
             "path": {"type": "string"},
+            "date": {"type": "string"},
         },
     }
 
-    # Context
-
-    @Plugin.property
-    def items(self):
-        return []
-
     # Process
+
+    @staticmethod
+    def process_project(self, project):
+        path = project.config.get("blog", {}).get("path", "blog")
+        if os.path.isfile(path):
+
+            # Add index
+            source = os.path.join(os.path.dirname(__file__), "index.md")
+            target = os.path.join(path, "index.html")
+            index = Document(source, target=target)
+            project.documents.append(index)
+
+            # Add items
+            for source in glob.glob(f"{self.path}/**/*.md"):
+                item = Document(source)
+                project.documents.append(item)
 
     def process_markup(self, markup):
         pass
