3ebb83e83f26663347d2f77477df3b5e158353d5
roll <eskarev@gmail.com>
2021-09-01 11:56:55 +0300

    Added docstrings to plugin

--- livemark/plugin.py
+++ livemark/plugin.py
@@ -33,30 +33,69 @@ class Plugin:
 
     @property
     def config(self):
+        """Plugin's config (empty if not provided)
+
+        Returns:
+          dict: config
+        """
         return self.__document.config.get(self.identity, {})
 
     @property
     def document(self):
+        """Plugin's document it belongs to
+
+        Returns:
+          Document: document
+        """
         return self.__document
 
     # Actions
 
     @staticmethod
     def process_project(project):
+        """Process project
+
+        Parameters:
+          project (Project): a project to process
+        """
         pass
 
     def process_document(self, document):
+        """Process document
+
+        Parameters:
+          document (Markup): a document to process
+        """
         pass
 
     def process_snippet(self, snippet):
+        """Process snippet
+
+        Parameters:
+          snippet (Markup): a snippet to process
+        """
         pass
 
     def process_markup(self, markup):
+        """Process markup
+
+        Parameters:
+          markup (Markup): a markup to process
+        """
         pass
 
     # Helpers
 
     def read_asset(self, *path, **context):
+        """Read plugin's asset
+
+        Parameters:
+          path (str[]): paths to join
+          context (dict): template variables
+
+        Returns:
+            str: a read asset
+        """
         project = self.document.project
         dir = os.path.dirname(inspect.getfile(self.__class__))
         path = os.path.join(dir, *path)
@@ -69,10 +108,18 @@ class Plugin:
 
     @classmethod
     def check_active(cls, config):
+        """Check whether the plugin is active in given config
+
+        Parameters:
+          config (Config): a config
+
+        Returns:
+          bool: whether active
+        """
         type = "external" if cls.__module__.startswith("livemark_") else "internal"
         internal = type == "internal" and cls.identity not in config.disabled
         external = type == "external" and cls.identity in config.enabled
         return internal or external
 
-    # TODO: review whether we need it
+    # TODO: review whether we need it (add cache arg or use property/cached_property?)
     property = cached_property
