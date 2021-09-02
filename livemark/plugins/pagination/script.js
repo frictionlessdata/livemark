abc6eda0fc506964e7bef72e0125806e713cacd7
roll <eskarev@gmail.com>
2021-09-02 10:15:34 +0300

    Remember readability setting

--- livemark/plugins/pagination/script.js
+++ livemark/plugins/pagination/script.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const conainer = $(".livemark-pagination");
-  const elements = conainer
-    .children()
-    .map((index, element) => element.outerHTML)
-    .get();
-  conainer.html(`
-    <div class="livemark-pagination-data"></div>
-    <div class="livemark-pagination-navs"></div>
-  `);
-  conainer.find(".livemark-pagination-navs").pagination({
-    dataSource: elements,
-    callback: (html) => {
-      conainer.find(".livemark-pagination-data").html(html);
-    },
-  });
-  conainer.show();
+  const container = $(".livemark-pagination");
+  if (container.length) {
+    const elements = container
+      .children()
+      .map((index, element) => element.outerHTML)
+      .get();
+    container.html(`
+      <div class="livemark-pagination-data"></div>
+      <div class="livemark-pagination-navs"></div>
+    `);
+    container.find(".livemark-pagination-navs").pagination({
+      dataSource: elements,
+      callback: (html) => {
+        container.find(".livemark-pagination-data").html(html);
+      },
+    });
+    container.show();
+  }
 });
