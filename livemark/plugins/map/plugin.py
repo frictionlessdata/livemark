27216362fa99900b2511489dbfaedf3cccbd1795
roll <eskarev@gmail.com>
2021-09-07 11:50:24 +0300

    Implemented MapPlugin

--- livemark/plugins/map/plugin.py
+++ livemark/plugins/map/plugin.py
@@ -1,6 +1,7 @@
 import json
 import yaml
 from ...plugin import Plugin
+from ... import helpers
 
 
 class MapPlugin(Plugin):
@@ -17,15 +18,14 @@ class MapPlugin(Plugin):
             if snippet.type == "map" and snippet.lang == "yaml":
                 spec_yaml = str(snippet.input).strip()
                 spec_python = yaml.safe_load(spec_yaml)
-                spec = json.dumps(spec_python, ensure_ascii=False)
+                spec = json.dumps(json.loads(helpers.read_file(spec_python["data"])))
                 spec = spec.replace("'", "\\'")
                 self.__count += 1
-                chart = {"spec": spec, "elem": f"livemark-chart-{self.__count}"}
-                snippet.output = self.read_asset("markup.html", chart=chart) + "\n"
+                map = {"spec": spec, "elem": f"livemark-map-{self.__count}"}
+                snippet.output = self.read_asset("markup.html", map=map) + "\n"
 
     def process_markup(self, markup):
         if self.__count:
             url = "https://unpkg.com"
-            markup.add_script(f"{url}/vega@5.20.2/build/vega.min.js")
-            markup.add_script(f"{url}/vega-lite@5.1.0/build/vega-lite.min.js")
-            markup.add_script(f"{url}/vega-embed@6.18.2/build/vega-embed.min.js")
+            markup.add_style(f"{url}/leaflet@1.7.1/dist/leaflet.css")
+            markup.add_script(f"{url}/leaflet@1.7.1/dist/leaflet.js")
