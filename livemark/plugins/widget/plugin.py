e2a39d91cc342c97061216f16fc78392af7fd81c
roll <eskarev@gmail.com>
2021-09-07 12:06:33 +0300

    Bootstrapped widget plugin

---
+++ livemark/plugins/widget/plugin.py
@@ -0,0 +1,13 @@
+from ...plugin import Plugin
+
+
+class WidgetPlugin(Plugin):
+    identity = "widget"
+
+    # Process
+
+    def process_snippet(self, snippet):
+        pass
+
+    def process_markup(self, markup):
+        pass
