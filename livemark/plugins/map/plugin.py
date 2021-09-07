8f37de6781d9d5814ff4c42845262385c310afa2
roll <eskarev@gmail.com>
2021-09-07 11:06:45 +0300

    Bootstrapped map plugin

---
+++ livemark/plugins/map/plugin.py
@@ -0,0 +1,29 @@
+import json
+import yaml
+from ...plugin import Plugin
+
+
+class MapPlugin(Plugin):
+    identity = "map"
+    priority = 60
+
+    # Process
+
+    def process_document(self, document):
+        self.__count = 0
+
+    def process_snippet(self, snippet):
+        if self.document.format == "html":
+            if snippet.type == "chart" and snippet.lang == "yaml":
+                spec_yaml = str(snippet.input).strip()
+                spec_python = yaml.safe_load(spec_yaml)
+                spec = json.dumps(spec_python, ensure_ascii=False)
+                spec = spec.replace("'", "\\'")
+                self.__count += 1
+                chart = {"spec": spec, "elem": f"livemark-chart-{self.__count}"}
+                snippet.output = self.read_asset("markup.html", chart=chart) + "\n"
+
+    def process_markup(self, markup):
+        markup.add_script("https://unpkg.com/vega@5.20.2/build/vega.min.js")
+        markup.add_script("https://unpkg.com/vega-lite@5.1.0/build/vega-lite.min.js")
+        markup.add_script("https://unpkg.com/vega-embed@6.18.2/build/vega-embed.min.js")
