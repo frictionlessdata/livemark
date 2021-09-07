a307176950e887bc4d1a0b21163e5451f922caba
roll <eskarev@gmail.com>
2021-09-07 09:41:06 +0300

    Improved audio plugin

--- docs/universe.md
+++ docs/universe.md
@@ -4,12 +4,17 @@
 
 [Test](#card=test)
 
+```yaml video
+type: native
+path: https://www.w3schools.com/html/mov_bbb.mp4
+```
+
 ```yaml audio
 type: soundcloud
 code: 34445479
 ```
 
-```yaml video
+```yaml audio
 type: native
-path: https://www.w3schools.com/html/mov_bbb.mp4
+path: https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3
 ```
--- livemark/plugins/audio/plugin.py
+++ livemark/plugins/audio/plugin.py
@@ -28,19 +28,13 @@ class AudioPlugin(Plugin):
         if self.document.format == "html":
             if snippet.type == "audio":
                 if snippet.lang == "yaml":
-                    spec_yaml = str(snippet.input).strip()
-                    spec_python = yaml.safe_load(spec_yaml)
-                    type = spec_python.get("type")
-                    code = spec_python.get("code")
-                    if type == "soundcloud" and code:
-                        width = spec_python.get("width", self.width)
-                        height = spec_python.get("height", self.height)
-                        snippet.output = self.read_asset(
-                            "markup.html",
-                            code=code,
-                            width=width,
-                            height=height,
-                        )
+                    context = yaml.safe_load(str(snippet.input).strip())
+                    context.setdefault("width", self.width)
+                    context.setdefault("height", self.height)
+                    if context["type"] == "native":
+                        snippet.output = self.read_asset("native.html", **context)
+                    elif context["type"] == "soundcloud":
+                        snippet.output = self.read_asset("soundcloud.html", **context)
 
     def process_markup(self, markup):
         markup.add_style("style.css")
