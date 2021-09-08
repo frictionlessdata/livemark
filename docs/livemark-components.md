924f5417553583a5e3d55d6566a6f53bce6af1ef
roll <eskarev@gmail.com>
2021-09-08 11:31:31 +0300

    Added JSX support to MarkupPlugin

--- docs/livemark-components.md
+++ docs/livemark-components.md
@@ -2,5 +2,13 @@
 
 ```yaml remark
 type: warning
-text: This page will be here shortly!
+text: It's a draft page for testing different features
+```
+
+```html markup
+<p>Hello World from HTML!</p>
+```
+
+```jsx markup
+return <p>Hello World from JSX!</p>
 ```
--- livemark/plugins/markup/plugin.py
+++ livemark/plugins/markup/plugin.py
@@ -13,12 +13,18 @@ from ...plugin import Plugin
 
 class MarkupPlugin(Plugin):
     identity = "markup"
+    priority = 60
 
     # Process
 
+    def process_document(self, document):
+        self.__jsx_count = 0
+
     def process_snippet(self, snippet):
         if self.document.format == "html":
-            if snippet.type == "markup":
+            if snippet.type == "markup" and snippet.lang in ["html", "jsx"]:
+
+                # Html
                 if snippet.lang == "html":
                     markdown = marko.Markdown()
                     markdown.use(GFM)
@@ -32,3 +38,18 @@ class MarkupPlugin(Plugin):
                             output = markdown.convert(input)
                             node.html(output)
                     snippet.output = query.outer_html() + "\n"
+
+                # Jsx
+                elif snippet.lang == "jsx":
+                    self.__jsx_count += 1
+                    context = {}
+                    context["content"] = snippet.input
+                    context["element"] = self.__jsx_count
+                    snippet.output = self.read_asset("markup.html", **context) + "\n"
+
+    def process_markup(self, markup):
+        if self.__jsx_count:
+            url = "https://unpkg.com"
+            markup.add_script(f"{url}/react@17.0.2/umd/react.production.min.js")
+            markup.add_script(f"{url}/react-dom@17.0.2/umd/react-dom.production.min.js")
+            markup.add_script(f"{url}/babel-standalone@6.26.0/babel.min.js")
