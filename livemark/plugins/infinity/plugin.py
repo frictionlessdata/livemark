0c1de13bcd8d4639af9059d356a5ae8e1e42cebc
roll <eskarev@gmail.com>
2021-09-08 10:24:20 +0300

    Implemented InfinityPlugin

---
+++ livemark/plugins/infinity/plugin.py
@@ -0,0 +1,16 @@
+from ...plugin import Plugin
+
+
+# NOTE:
+# Add support for many infinity blocks on page
+# Add load more button and loading indication?
+
+
+class InfinityPlugin(Plugin):
+    identity = "infinity"
+
+    # Process
+
+    def process_markup(self, markup):
+        markup.add_style("style.css")
+        markup.add_script("script.js")
