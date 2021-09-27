06d889ef03ee4db7927caf599db4947adb715500
roll <eskarev@gmail.com>
2021-09-27 10:51:51 +0300

    Fixed tilte in CardsPlugin

--- livemark/plugins/cards/script.js
+++ livemark/plugins/cards/script.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // TODO: handle errors
         const response = await fetch(`/assets/cards/${code}.html`);
         const html = await response.text();
+        $("#livemark-cards .modal-title").html("");
         $("#livemark-cards .modal-body").html(html);
         $("#livemark-cards h1").appendTo("#livemark-cards .modal-title");
         $("#livemark-cards .modal").modal();
