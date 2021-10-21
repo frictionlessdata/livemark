0fcbc800a40423a47d85be737d1c45d5662ad993
roll <eskarev@gmail.com>
2021-10-21 10:40:13 +0300

    Added "Show the Source" button

--- livemark/plugins/source/script.js
+++ livemark/plugins/source/script.js
@@ -1,1 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  for (const h2 of $("h2")) {
+    $(h2).append(
+      '<a href="" class="livemark-source-button">Show the Source</a>'
+    );
+  }
+});
